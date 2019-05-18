60ada9b162bbc7c57508e6c1d43826a069c31b5c
yummyJade <yuyuan7@126.com>
2019-05-18 11:11:00 +0800

    just to keep

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,4 +1,5 @@
 // pages/test/test.js
+import Test from '../../utils/test'; 
 Page({
 
   /**
@@ -26,7 +27,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+   console.log( Test(2,5));
   },
 
   /**
